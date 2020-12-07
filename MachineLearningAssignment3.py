8d7b8a90ebe67f576bdb92314450da68fe42f5c8
Cian O'Sullivan <cjp1805@outlook.com>
2020-12-07 14:59:20 +0000

    Adding dataset and blank python file

---
+++ MachineLearningAssignment3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 14:54:03 2020
+
+@author: Cian
+"""
+
+# carat
+# depth
+# table
+# selling price
+# assignment data points and features maybe?
+# Somehting about groups and then work on groups
+# Combinations is groups
+
